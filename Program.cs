2b5b8e50e6463b5d59ff5c4a91c8096e3ae75fbd
Leonardodognani <82122343+Leonardodognani@users.noreply.github.com>
2021-09-15 23:05:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo Aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case 1:
+                        //todo add aluno
+                    break;
+
+                    case 2:
+                        //todo listar alunos
+                    break;
+
+                    case 3:
+                        //todo calcular media geral
+                    break;
+
+                default:
+                    throw new argumentOutOfRangeException();
+                }
+
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo Aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            opcaoUsuario = Console.ReadLine();
+            }
+
+        }
+
+    }
+}
