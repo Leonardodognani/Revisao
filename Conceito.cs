24c151185688b597ff4f879a6d3ef091a55be464
Leonardodognani <82122343+Leonardodognani@users.noreply.github.com>
2021-09-16 21:48:59 -0300

    final commit

---
+++ Conceito.cs
@@ -0,0 +1,15 @@
+namespace Revisao
+{
+    public enum Conceito
+    {
+        A,
+
+        B,
+
+        C,
+
+        D,
+
+        E
+    }
+}
--- Program.cs
+++ Program.cs
@@ -18,11 +18,11 @@ namespace Revisao
                     case "1":
                         Console.WriteLine("Informe o nome do aluno:");
                         Aluno aluno = new Aluno();
-                        aluno.Nome = COnsole.ReadLine();
+                        aluno.Nome = Console.ReadLine();
 
                         Console.WriteLine("Informe a Nota do aluno:");
                         
-                        if (decimal.TryParse(Console.ReadLine(), out decimal nota));
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
                         {
                             aluno.Nota = nota;
                         }
@@ -37,11 +37,57 @@ namespace Revisao
                     break;
 
                     case "2":
-                        //todo listar alunos
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                            
+                        }
                     break;
 
                     case "3":
-                        //todo calcular media geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i = 0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+                        
+
+                        Console.WriteLine($"MEDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+
+
                     break;
 
                     default:
@@ -56,6 +102,7 @@ namespace Revisao
 
         private static string obterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo Aluno");
             Console.WriteLine("2 - Listar Alunos");
